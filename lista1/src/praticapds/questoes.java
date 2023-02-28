68aa3be5f2bccdf33dedc78d7d435888b93503fc
Maria Tereza <mariawippel05@gmail.com>
2023-02-28 09:08:23 -0300

    Exerc.Prática
    Aula 28/02 (1 e 2)

---
+++ lista1/src/praticapds/questoes.java
@@ -0,0 +1,16 @@
+package praticapds;
+
+//1. Faça um programa que lê precoLitro (tipo Double) e quantidadeLitros (tipo Integer) usando o método showInputDialog 
+//da classe JOptionPane. Crie um método (função) que calcule o totalPagamento e use ele para calcular os valores inseridos.
+//Exiba o valor final utilizando o método showMessageDialog da classe JOptionPane.
+//Crie uma lista (ArrayList) de String. Preencha 10 (dez) elementos nesta lista fazendo a leitura com o método showInputDialog 
+//da classe JOptionPane em um for array. Em seguida, exiba todos os dados de uma só vez utilizando o método showMessageDialog 
+//da classe JOptionPane.
+
+	public class questoes{
+	
+	public static void main(String[] args){
+	
+	}
+	
+}
